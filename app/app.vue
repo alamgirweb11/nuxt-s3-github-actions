03be946d56cef5abfb1b0cbca352454e7c609561
alamgirweb11 <alamgirweb11@gmail.com>
2025-08-22 21:00:35 +0600

    fresh template

---
+++ app/app.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="min-h-screen flex items-center justify-center bg-gradient-to-r from-slate-900 to-slate-700">
+    <h1 class="text-3xl font-bold text-center bg-gradient-to-r from-blue-600 to-yellow-400 bg-clip-text text-transparent">
+     Nuxt app successfully deployed to Amazon S3 using GitHub Actions!
+  </h1>
+  </div>
+</template>
