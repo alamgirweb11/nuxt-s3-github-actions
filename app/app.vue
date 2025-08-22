c10c0d299ba39808f6253ad62863e4d62d785a72
alamgirweb11 <alamgirweb11@gmail.com>
2025-08-22 23:03:54 +0600

    add text for checking deployment status

--- app/app.vue
+++ app/app.vue
@@ -2,6 +2,8 @@
   <div class="min-h-screen flex items-center justify-center bg-gradient-to-r from-slate-900 to-slate-700">
     <h1 class="text-3xl font-bold text-center bg-gradient-to-r from-blue-600 to-yellow-400 bg-clip-text text-transparent">
      Nuxt App successfully deployed to Amazon S3 using GitHub Actions!
+     <br>
+     Deploy status: <span class="text-green-500">Success</span>
   </h1>
   </div>
 </template>
